cfddf40eabd6cf35d62a216fd706baec2982ee7f
davidayman11 <129064507+davidayman11@users.noreply.github.com>
2025-08-30 12:38:48 +0300

    Update team_members.php

--- team_members.php
+++ team_members.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 require_once 'db.php';
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $team = isset($_GET['team']) ? $conn->real_escape_string($_GET['team']) : '';
 if (!$team) die("No team specified.");
 
